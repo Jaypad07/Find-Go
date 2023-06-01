2bfb84ceaf191352ba1831c4d8bd2fea9c9de68a
jaypad07 <jaypad07@gmail.com>
2023-06-01 09:00:51 -0700

    setup cucumber

---
+++ src/main/java/com/sei/findgo/exceptions/InformationExistException.java
@@ -0,0 +1,14 @@
+package com.sei.findgo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * useful to have custom error messages to be more informative for developer and/or user
+ */
+@ResponseStatus(HttpStatus.CONFLICT)
+public class InformationExistException extends RuntimeException{
+    public InformationExistException(String message){
+        super(message);
+    }
+}
