f2ca06c2ffd2f2f6433189a0e282c6d7958cbad1
jaypad07 <jaypad07@gmail.com>
2023-06-01 08:20:19 -0700

    second commit

---
+++ src/main/java/com/sei/findgo/FindGoApplication.java
@@ -0,0 +1,13 @@
+package com.sei.findgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sei/findgo/FindGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sei.findgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
