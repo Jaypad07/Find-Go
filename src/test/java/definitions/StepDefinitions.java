8d57e2dfe8e30482c382a70dd7dc0b79a72a4986
jaypad07 <jaypad07@gmail.com>
2023-06-03 16:21:44 -0700

    Test failed - iEnterValidLoginCredentialsUsernamePassword

--- src/test/java/definitions/StepDefinitions.java
+++ src/test/java/definitions/StepDefinitions.java
@@ -2,7 +2,6 @@ package definitions;
 
 import com.sei.findgo.FindGoApplication;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
@@ -36,25 +35,39 @@ public class StepDefinitions {
     }
 
 
-    @Given("I am on the registration page")
-    public void iAmOnTheRegistrationPage() throws JSONException {
+//    @Given("I am on the registration page")
+//    public void iAmOnTheRegistrationPage() throws JSONException {
+//    }
+//
+//    @When("I enter valid registration details \\(username, email, password)")
+//    public void iEnterValidRegistrationDetailsUsernameEmailPassword() throws JSONException {
+//        RequestSpecification request = RestAssured.given();
+//        JSONObject requestBody = new JSONObject();
+//        requestBody.put("username", "example34");
+//        requestBody.put("email", "example34@example.com");
+//        requestBody.put("password", "password5");
+//        request.header("Content-Type", "application/json");
+//        response = request.body(requestBody.toString()).post(BASE_URL + port + "/api/users/register");
+//        Assert.assertEquals(200, response.getStatusCode());
+//    }
+//
+//    @Then("I should be successfully registered")
+//    public void iShouldBeSuccessfullyRegistered() throws JSONException {
+//        Assert.assertEquals(200, response.getStatusCode());
+//    }
+
+    @Given("I am on the login page")
+    public void iAmOnTheLoginPage() {
     }
 
-    @When("I enter valid registration details \\(username, email, password)")
-    public void iEnterValidRegistrationDetailsUsernameEmailPassword() throws JSONException {
+    @When("I enter valid login credentials \\(username, password)")
+    public void iEnterValidLoginCredentialsUsernamePassword() throws JSONException {
         RequestSpecification request = RestAssured.given();
         JSONObject requestBody = new JSONObject();
-        requestBody.put("username", "example34");
-        requestBody.put("email", "example34@example.com");
+        requestBody.put("email", "jay@example.com");
         requestBody.put("password", "password5");
         request.header("Content-Type", "application/json");
-        response = request.body(requestBody.toString()).post(BASE_URL + port + "/api/users/register");
-        Assert.assertEquals(200, response.getStatusCode());
+        response = request.body(requestBody.toString()).post(BASE_URL + port + "/api/users/login");
+        Assert.assertEquals(400, response.getStatusCode());
     }
-
-    @Then("I should be successfully registered")
-    public void iShouldBeSuccessfullyRegistered() throws JSONException {
-        Assert.assertEquals(200, response.getStatusCode());
-    }
-
 }
--- src/test/resources/features/User.feature
+++ src/test/resources/features/User.feature
@@ -1,5 +1,12 @@
-Feature: User able to register
-  Scenario: User Registration
-    Given I am on the registration page
-    When I enter valid registration details (username, email, password)
-    Then I should be successfully registered
+#Feature: User able to register
+#  Scenario: User Registration
+#    Given I am on the registration page
+#    When I enter valid registration details (username, email, password)
+#    Then I should be successfully registered
+
+  Feature: User able to login
+Scenario: User Login
+  Given I am on the login page
+  When I enter valid login credentials (username, password)
+#  Then I should be logged in successfully
+
