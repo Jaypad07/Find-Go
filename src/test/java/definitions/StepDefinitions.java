4057dae6b7e8ee84a2c75ebb3c5186bd99617741
jaypad07 <jaypad07@gmail.com>
2023-06-03 15:33:56 -0700

    Test failing - IamOnTheRegistrationPage

--- src/test/java/definitions/StepDefinitions.java
+++ src/test/java/definitions/StepDefinitions.java
@@ -2,12 +2,14 @@ package definitions;
 
 import com.sei.findgo.FindGoApplication;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -28,20 +30,30 @@ public class StepDefinitions {
         jsonObject.put("email", "obinna@example.com");
         jsonObject.put("password", "password3");
         request.header("Content-Type", "application/json");
-        response = request.body(jsonObject.toString()).post(BASE_URL + port + "/api/auth/users/login");
+        response = request.body(jsonObject.toString()).post(BASE_URL + port + "/api/users/login");
         return response.jsonPath().getString("message");
     }
     @Given("I am on the registration page")
-    public void iAmOnTheRegistrationPage() {
+    public void iAmOnTheRegistrationPage() throws JSONException {
         RequestSpecification request = RestAssured.given();
-        request.header("Content-Type", "application/json");
-        response = request.get(BASE_URL + port + "/api/users/register");
-        response.then().statusCode(200);
+//        JSONObject requestBody = new JSONObject();
+//        requestBody.put("email", "example34@example.com");
+//        requestBody.put("password", "password5");
+//        request.header("Content-Type", "application/json");
+//        response = request.body(requestBody.toString()).post(BASE_URL + port + "/api/users/register");
+        Assert.assertEquals(200, response.getStatusCode());
     }
 
 
-
-
-
-
+//    @When("I enter valid registration details \\(username, email, password)")
+//    public void iEnterValidRegistrationDetailsUsernameEmailPassword() throws JSONException {
+//        RequestSpecification request = RestAssured.given();
+//        JSONObject requestBody = new JSONObject();
+//        requestBody.put("username", "example34");
+//        requestBody.put("email", "example34@example.cinm");
+//        requestBody.put("password", "password5");
+//        request.header("Content-Type", "application/json");
+//        response = request.body(requestBody.toString()).post(BASE_URL + port + "/api/users/register");
+//        Assert.assertEquals(200, response.getStatusCode());
+//    }
 }
--- src/test/resources/features/User.feature
+++ src/test/resources/features/User.feature
@@ -2,5 +2,4 @@ Feature: User able to register
   Scenario: User Registration
     Given I am on the registration page
 #    When I enter valid registration details (username, email, password)
-#    And I submit the registration form
 #    Then I should be successfully registered
