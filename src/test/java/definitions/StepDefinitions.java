3fec405028d4d6e16fa77e8e83e25d842a645666
jaypad07 <jaypad07@gmail.com>
2023-06-03 17:06:51 -0700

    Test failing - thereAreMultipleUsersInTheSystem

--- src/test/java/definitions/StepDefinitions.java
+++ src/test/java/definitions/StepDefinitions.java
@@ -1,11 +1,13 @@
 package definitions;
 
 import com.sei.findgo.FindGoApplication;
+import com.sei.findgo.models.User;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.JSONException;
@@ -13,6 +15,13 @@ import org.json.JSONObject;
 import org.junit.Assert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = FindGoApplication.class)
@@ -25,11 +34,16 @@ public class StepDefinitions {
 
     private static Response response;
 
+    private static ResponseEntity<String> responseEntity;
+    private static List<?> list;
+
+    User user = new User("John",  "Doe@example.com", "password", "User");
+
     public String JWTTestKey() throws JSONException {
         RequestSpecification request = RestAssured.given();
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("email", "obinna@example.com");
-        jsonObject.put("password", "password3");
+        jsonObject.put("email", "jay@example.com");
+        jsonObject.put("password", "password5");
         request.header("Content-Type", "application/json");
         response = request.body(jsonObject.toString()).post(BASE_URL + port + "/api/users/login");
         return response.jsonPath().getString("message");
@@ -74,6 +88,16 @@ public class StepDefinitions {
 
     @Then("I should be logged in successfully")
     public void iShouldBeLoggedInSuccessfully() {
+        Assert.assertEquals(200, response.getStatusCode());
+    }
+
+    @Given("there are multiple users in the system")
+    public void thereAreMultipleUsersInTheSystem() throws JSONException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(JWTTestKey());
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        responseEntity = new RestTemplate().exchange(BASE_URL + port + "/auth/users", HttpMethod.GET, entity, String.class);
+        list = JsonPath.from(String.valueOf(responseEntity.getBody())).get();
         Assert.assertEquals(200, response.getStatusCode());
     }
 }
--- src/test/resources/features/User.feature
+++ src/test/resources/features/User.feature
@@ -9,3 +9,13 @@ Scenario: User Login
   When I enter valid login credentials (username, password)
   Then I should be logged in successfully
 
+  Scenario: Get All Users
+    Given there are multiple users in the system
+#    When the client requests to get all users
+#    Then the response should contain a list of all users
+
+#  Scenario: Get User by ID
+#    Given a user with ID {userId}
+#    When the client requests to get the user by ID {userId}
+#    Then the response should contain the user details
+
