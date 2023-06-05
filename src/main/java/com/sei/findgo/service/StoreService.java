a5b60f9e36c7524e6df90d05799421c6a6f61485
jaypad07 <jaypad07@gmail.com>
2023-06-04 23:06:21 -0700

    updated StoreSection to check for empty/null name

--- src/main/java/com/sei/findgo/service/StoreService.java
+++ src/main/java/com/sei/findgo/service/StoreService.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 
 /**
  * Service class for managing stores and store sections.
- *
+ * <p>
  * This class provides methods to perform operations on stores and store sections.
  * It interacts with the {@link StoreRepository} to access and manipulate store data.
  * The service methods implement various business logic and authorization checks.
@@ -58,12 +58,12 @@ public class StoreService {
 
     /**
      * Retrieves all store sections.
-     *
+     * <p>
      * This method retrieves all store sections from the database.
      *
      * @return a list of store sections
      * @throws InformationNotFoundException if no store sections exist
-     * @throws NoAuthorizationException if the user is not authorized to perform this action
+     * @throws NoAuthorizationException     if the user is not authorized to perform this action
      */
     public List<StoreSection> getAllStoreSections() {
         Optional<User> user = Optional.ofNullable(UserService.getCurrentLoggedInUser());
@@ -72,50 +72,50 @@ public class StoreService {
             if (storeSectionList.size() > 0) {
                 return storeSectionList;
             } else throw new InformationNotFoundException("No store sections exist");
-        }else throw new NoAuthorizationException("You are not authorized to perform this action");
+        } else throw new NoAuthorizationException("You are not authorized to perform this action");
     }
 
     /**
      * Adds a new store section to a store.
      *
-     * @param storeId The ID of the store to add the section to.
+     * @param storeId            The ID of the store to add the section to.
      * @param storeSectionObject The store section object to be added.
      * @return The added store section.
      * @throws InformationNotFoundException If the store with the specified ID does not exist.
-     * @throws NoAuthorizationException If the user is not authorized to add the store section.
-     * @throws InformationExistException If a store section with the same name already exists.
+     * @throws NoAuthorizationException     If the user is not authorized to add the store section.
+     * @throws InformationExistException    If a store section with the same name already exists.
      */
     public StoreSection addStoreSection(int storeId, StoreSection storeSectionObject) {
         Optional<User> user = Optional.ofNullable(UserService.getCurrentLoggedInUser());
         if (user.isPresent() && user.get().getRole().equals("Admin") || user.isPresent() && user.get().getRole().equals("Manager")) {
             Optional<Store> store = storeRepository.findById(storeId);
             if (store.isPresent()) {
-               Optional<StoreSection> storeSection = Optional.ofNullable(storeSectionRepository.findStoreSectionBySectionNameIgnoreCase(storeSectionObject.getSectionName()));
-               if (storeSection.isPresent()) {
-                   throw new InformationExistException("A store section with the same name already exists");
-               } else {
-                   store.get().getStoreSectionsList().add(storeSectionObject);
-                   storeSectionObject.setStore(store.get());
-                   storeSectionRepository.save(storeSectionObject);
-                   return storeSectionObject;
-               }
-            } else {
-                throw new InformationNotFoundException("The store you are looking for does not exist");
-            }
-        } else {
-            throw new NoAuthorizationException("You are not authorized to perform this action");
-        }
+                if (storeSectionObject.getSectionName() == null || storeSectionObject.getSectionName().isEmpty()) {
+                    throw new InformationExistException("The store section name cannot be empty");
+                } else {
+                    Optional<StoreSection> storeSection = Optional.ofNullable(storeSectionRepository.findStoreSectionBySectionNameIgnoreCase(storeSectionObject.getSectionName()));
+                    if (storeSection.isPresent()) {
+                        throw new InformationExistException("A store section with the same name already exists");
+                    } else {
+                        store.get().getStoreSectionsList().add(storeSectionObject);
+                        storeSectionObject.setStore(store.get());
+                        storeSectionRepository.save(storeSectionObject);
+                        return storeSectionObject;
+                    }
+                }
+            } else throw new InformationNotFoundException("The store you are looking for does not exist");
+        } else throw new NoAuthorizationException("You are not authorized to perform this action");
     }
 
     /**
      * Updates a store section.
      *
-     * @param storeId The ID of the store that contains the section.
-     * @param storeSectionId The ID of the store section to update.
+     * @param storeId            The ID of the store that contains the section.
+     * @param storeSectionId     The ID of the store section to update.
      * @param storeSectionObject The updated store section object.
      * @return The updated store section.
      * @throws InformationNotFoundException If the store with the specified ID does not exist.
-     * @throws NoAuthorizationException If the user is not authorized to update the store section.
+     * @throws NoAuthorizationException     If the user is not authorized to update the store section.
      */
     public StoreSection updateStoreSection(int storeId, int storeSectionId, StoreSection storeSectionObject) {
         Optional<User> user = Optional.ofNullable(UserService.getCurrentLoggedInUser());
@@ -203,7 +203,7 @@ public class StoreService {
     /**
      * Updates a store.
      *
-     * @param storeId The ID of the store to update.
+     * @param storeId     The ID of the store to update.
      * @param storeObject The updated store object.
      * @return The updated store.
      * @throws InformationNotFoundException If the store with the specified ID does not exist.
@@ -233,7 +233,7 @@ public class StoreService {
      *
      * @param storeId The ID of the store to delete.
      * @return The deleted store.
-     * @throws ProductNotFoundException  If the store with the specified ID does not exist.
+     * @throws ProductNotFoundException If the store with the specified ID does not exist.
      * @throws NoAuthorizationException If the user is not authorized to delete the store.
      */
     public Store deleteStore(int storeId) {
