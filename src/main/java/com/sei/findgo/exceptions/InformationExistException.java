b57aef9c5f99a103f0d3d0743194a51d5665eb8c
jaypad07 <jaypad07@gmail.com>
2023-06-04 17:44:47 -0700

    removed exception

--- src/main/java/com/sei/findgo/exceptions/InformationExistException.java
+++ src/main/java/com/sei/findgo/exceptions/InformationExistException.java
@@ -3,9 +3,7 @@ package com.sei.findgo.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- * useful to have custom error messages to be more informative for developer and/or user
- */
+
 @ResponseStatus(HttpStatus.CONFLICT)
 public class InformationExistException extends RuntimeException{
     public InformationExistException(String message){
